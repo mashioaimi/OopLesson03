fcc6836b46507a939997d3204d17f3741bd3b8f4
AimiMashio <31083@ojs.ac.jp>
2020-09-28 15:29:47 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Chapter3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>
+            {
+                "Tokyo","New Delhi","Bangkok","London","Paris","Berlin","Canberra","Hong Kong"
+            };
+            var query = names.Where(s => s.StartsWith("B")).Select(s => s.Length);
+            foreach (var length in query)
+            {
+                Console.WriteLine(length);
+            }
+        }
+    }
+}
