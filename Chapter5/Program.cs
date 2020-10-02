1dd61bde45be025295962b7ba2e5d60d961f6142
AimiMashio <31083@ojs.ac.jp>
2020-10-02 15:53:14 +0900

    演習5.2

--- Chapter5/Program.cs
+++ Chapter5/Program.cs
@@ -12,7 +12,9 @@ namespace Chapter5
         static void Main(string[] args)
         {
             //5.1
+            Console.Write("文字列１：");
             var str1 = Console.ReadLine();
+            Console.Write("文字列２：");
             var str2 = Console.ReadLine();
             if (String.Compare(str1,str2,true) == 0)
             {
@@ -21,6 +23,21 @@ namespace Chapter5
             else
             {
                 Console.WriteLine("等しくない");
+            }
+
+            //5.2
+            Console.Write("数値文字列：");
+            var line = Console.ReadLine();
+            int num;    //返還後の数値格納用
+            if(int.TryParse(line, out num))
+            {
+                Console.WriteLine(num.ToString("#,#"));
+                //Console.WriteLine("{#,#}", num);  この二つでもよい
+                //Console.WriteLine($"{num:#,#}");
+            }
+            else
+            {
+                Console.WriteLine("数値文字列ではありません");
             }
         }
     }
