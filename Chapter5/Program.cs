7cb8080eec0fa6a672946495db644c912f4f39de
AimiMashio <31083@ojs.ac.jp>
2020-10-02 15:33:39 +0900

    演習5.1

---
+++ Chapter5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //5.1
+            var str1 = Console.ReadLine();
+            var str2 = Console.ReadLine();
+            if (String.Compare(str1,str2,true) == 0)
+            {
+                Console.WriteLine("等しい");
+            }
+            else
+            {
+                Console.WriteLine("等しくない");
+            }
+        }
+    }
+}
