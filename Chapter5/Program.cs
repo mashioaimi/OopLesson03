1267e29560ae72ce64090a8aac4a82e3dcae8af2
AimiMashio <31083@ojs.ac.jp>
2020-10-05 15:10:02 +0900

    5.4

--- Chapter5/Program.cs
+++ Chapter5/Program.cs
@@ -11,8 +11,8 @@ namespace Chapter5
     {
         static void Main(string[] args)
         {
-            //var text = "Jackdaws love my big sphinx of quartz";
-            //var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;born=1886";
+            var text = "Jackdaws love my big sphinx of quartz";
+            var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;born=1886";
 
             #region 5.1
             //5.1
@@ -50,67 +50,67 @@ namespace Chapter5
             #endregion
 
             #region 5.3
-            //5.3
-            var text = "Jackdaws love my big sphinx of quartz";
-            //5.3.1
-            Console.WriteLine("----5.3.1----");
-            Console.WriteLine(text.Count(s => s == ' '));
-            
-            //5.3.2
-            Console.WriteLine("----5.3.2----");
-            var replaced = text.Replace("big", "small");
-            Console.WriteLine(replaced);
-            
-            //5.3.3
-            Console.WriteLine("----5.3.3----");
-            var count = text.Split(' ').Count();
-            Console.WriteLine(count);
-            
-            //5.3.4
-            Console.WriteLine("----5.3.4----");
-            var words = text.Split(' ').Where(s => s.Length <= 4);
-            foreach (var word in words)
-            {
-                Console.WriteLine(word);
-            }
-            
-            //5.3.5
-            Console.WriteLine("----5.3.5----");
-            var array = text.Split(' ').ToArray();
-            if (array.Length >= 0)
-            {
-                var sb = new StringBuilder(array[0]);
-                foreach (var word in array.Skip(1))
-                {
-                    sb.Append(' ');
-                    sb.Append(word);
-                }
-                Console.WriteLine(sb);
-            }
+            ////5.3
+            //var text = "Jackdaws love my big sphinx of quartz";
+            ////5.3.1
+            //Console.WriteLine("----5.3.1----");
+            //Console.WriteLine(text.Count(s => s == ' '));
+            //
+            ////5.3.2
+            //Console.WriteLine("----5.3.2----");
+            //var replaced = text.Replace("big", "small");
+            //Console.WriteLine(replaced);
+            //
+            ////5.3.3
+            //Console.WriteLine("----5.3.3----");
+            //var count = text.Split(' ').Count();
+            //Console.WriteLine(count);
+            //
+            ////5.3.4
+            //Console.WriteLine("----5.3.4----");
+            //var words = text.Split(' ').Where(s => s.Length <= 4);
+            //foreach (var word in words)
+            //{
+            //    Console.WriteLine(word);
+            //}
+            //
+            ////5.3.5
+            //Console.WriteLine("----5.3.5----");
+            //var array = text.Split(' ').ToArray();
+            //if (array.Length >= 0)
+            //{
+            //    var sb = new StringBuilder(array[0]);
+            //    foreach (var word in array.Skip(1))
+            //    {
+            //        sb.Append(' ');
+            //        sb.Append(word);
+            //    }
+            //    Console.WriteLine(sb);
+            //}
             #endregion
 
             #region
             //5.4
-            //    Console.WriteLine("----5.4----");
-            //    foreach (var item in line.Split(';'))
-            //    {
-            //        var word = item.Split('=');
-            //        Console.WriteLine("{0}:{1}", ToJapanese(word[0]), word[1]);
-            //    }
-            //}
-            //static string ToJapanese(string key)
-            //{
-            //    switch (key)
-            //    {
-            //        case "Novelist":
-            //            return "作家　";
-            //        case "BestWork":
-            //            return "代表作";
-            //        case "Born":
-            //            return "誕生年";
-            //        default:
-            //            return "　　　";
-            //    }
+                Console.WriteLine("----5.4----");
+                foreach (var item in line.Split(';'))
+                {
+                    var word = item.Split('=');
+                    Console.WriteLine("{0}:{1}", ToJapanese(word[0]), word[1]);
+                }
+            }
+            static string ToJapanese(string key)
+            {
+                switch (key)
+                {
+                    case "Novelist":
+                        return "作家　";
+                    case "BestWork":
+                        return "代表作";
+                    case "Born":
+                        return "誕生年";
+                    default:
+                        return "　　　";
+                }
             #endregion
 
         }
